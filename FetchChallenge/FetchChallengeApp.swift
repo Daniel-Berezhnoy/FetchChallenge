dae02192ddda814829779263e8fa83842a8e4781
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2024-06-28 15:27:20 -0700

    Initial Commit

---
+++ FetchChallenge/FetchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchChallengeApp.swift
+//  FetchChallenge
+//
+//  Created by Daniel Berezhnoy on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
