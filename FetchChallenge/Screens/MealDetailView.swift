042f6e2a65a22b78970f974d68ffd0e5dcc178bb
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2024-06-30 23:25:46 -0700

    Added image as a background

--- FetchChallenge/Screens/MealDetailView.swift
+++ FetchChallenge/Screens/MealDetailView.swift
@@ -12,7 +12,6 @@ struct MealDetailView: View {
     
     var body: some View {
         ScrollView {
-            
             VStack(alignment: .leading) {
                 image
                 titleSubtitle
@@ -22,8 +21,10 @@ struct MealDetailView: View {
         }
         .navigationTitle("Details")
         .toolbarTitleDisplayMode(.inline)
+        .background { backgroundImage }
     }
     
+    #warning("Blur the edges")
     private var image: some View {
         AsyncImage(url: URL(string: meal.imageURL)) { image in
             image
@@ -80,6 +81,17 @@ struct MealDetailView: View {
         }
         .padding(.horizontal)
         .padding(.top)
+    }
+    
+    private var backgroundImage: some View {
+        AsyncImage(url: URL(string: meal.imageURL)) { image in
+            image.image?
+                .resizable()
+                .scaledToFill()
+        }
+        .opacity(0.4)
+        .blur(radius: 20)
+        .ignoresSafeArea(.all, edges: .all)
     }
     
     let instructions = "Heat oven to 180C/160C fan/gas 4 and line the base and sides of a 20cm square tin with baking parchment (the easiest way is to cross 2 x 20cm-long strips over the base). To make the almond sponge, put the butter, sugar, flour, ground almonds, baking powder, eggs, vanilla and almond extract in a large bowl. Beat with an electric whisk until the mix comes together smoothly. Scrape into the tin, spreading to the corners, and bake for 25-30 mins 2013 when you poke in a skewer, it should come out clean. Cool in the tin for 10 mins, then transfer to a wire rack to finish cooling while you make the second sponge.\r\nFor the pink"
