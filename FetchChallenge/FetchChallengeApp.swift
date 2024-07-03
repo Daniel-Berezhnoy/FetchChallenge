6146bb062dfd8f036db9bba9c4dd0b1f6bd8f9b9
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2024-07-03 15:00:08 -0700

    Added TabView

--- FetchChallenge/FetchChallengeApp.swift
+++ FetchChallenge/FetchChallengeApp.swift
@@ -16,40 +16,9 @@ import SwiftUI
 
 @main
 struct FetchChallengeApp: App {
-    
-    @State private var accentColor = Color.blue
-    @State private var showID = true
-    
     var body: some Scene {
         WindowGroup {
-            TabView {
-                menu
-                settings
-            }
-            .tint(accentColor)
+            FCTabView()
         }
-    }
-    
-    // Screens
-    private var menu: some View {
-        MenuView(showID: $showID, accentColor: $accentColor)
-            .tabItem {
-                Label {
-                    Text("Menu")
-                } icon: {
-                    Image(systemName: "takeoutbag.and.cup.and.straw")
-                }
-            }
-    }
-    
-    private var settings: some View {
-        SettingsView(showID: $showID, accentColor: accentColor)
-            .tabItem {
-                Label {
-                    Text("Settings")
-                } icon: {
-                    Image(systemName: "gear")
-                }
-            }
     }
 }
---
+++ FetchChallenge/Screens/FCTabView.swift
@@ -0,0 +1,49 @@
+//
+//  FCTabView.swift
+//  FetchChallenge
+//
+//  Created by Daniel Berezhnoy on 7/3/24.
+//
+
+import SwiftUI
+
+struct FCTabView: View {
+    
+    @State private var accentColor = Color.blue
+    @State private var showID = true
+    
+    var body: some View {
+        TabView {
+            menu
+            settings
+        }
+        .tint(accentColor)
+    }
+    
+    // Screens
+    private var menu: some View {
+        MenuView(showID: $showID, accentColor: $accentColor)
+            .tabItem {
+                Label {
+                    Text("Menu")
+                } icon: {
+                    Image(systemName: "takeoutbag.and.cup.and.straw")
+                }
+            }
+    }
+    
+    private var settings: some View {
+        SettingsView(showID: $showID, accentColor: accentColor)
+            .tabItem {
+                Label {
+                    Text("Settings")
+                } icon: {
+                    Image(systemName: "gear")
+                }
+            }
+    }
+}
+
+#Preview {
+    FCTabView()
+}
